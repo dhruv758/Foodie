515b1656464d0ba93bf828f8e3202e325a2379e9
Priyanshu Gupta <priyanshugupta.112002@gmail.com>
2025-03-28 14:38:28 +0530

    changes in the navbar so that button become active

--- frontend/src/HomePage/components/HomeNavbar.jsx
+++ frontend/src/HomePage/components/HomeNavbar.jsx
@@ -2,35 +2,60 @@ import logo from "../../assets/logo.png";
 import location from "../../assets/location.svg";
 import list from "../../assets/list.svg";
 import profile from "../../assets/profile.svg";
+import { NavLink } from "react-router-dom";
 
 function HomeNavbar() {
   return (
     <div className="outer flex justify-between px-16 pb-2">
+      {/* Left Section */}
       <div className="left flex items-center gap-10">
         <img src={logo} className="w-48 h-10 mt-2" />
         <div className="location flex items-center cursor-pointer">
-          <img src={location} alt="" className="w-[35px] h-[35px]" />
+          <img src={location} alt="Location" className="w-[35px] h-[35px]" />
           <h1 className="text-lg font-semibold">Ncr Sec</h1>
         </div>
       </div>
+
+      {/* Center Section - Navigation Links */}
       <div className="center flex gap-10 items-center">
-        <h1 className="text-xl font-semibold cursor-pointer hover:scale-110 hover:text-[#1ac073]">
+        <NavLink
+          to="/home"
+          className={({ isActive }) =>
+            `text-xl font-semibold cursor-pointer hover:scale-110 ${
+              isActive ? "text-[#1ac073]" : "hover:text-[#1ac073]"
+            }`
+          }
+        >
           Home
-        </h1>
-        <h1 className="text-xl font-semibold cursor-pointer hover:scale-110 hover:text-[#1ac073]">
+        </NavLink>
+
+        <NavLink
+          to="/summary"
+          className={({ isActive }) =>
+            `text-xl font-semibold cursor-pointer hover:scale-110 ${
+              isActive ? "text-[#1ac073]" : "hover:text-[#1ac073]"
+            }`
+          }
+        >
           Summary
-        </h1>
-        <h1 className="text-xl font-semibold cursor-pointer hover:scale-110 hover:text-[#1ac073]">
+        </NavLink>
+
+        <NavLink
+          to="/polls"
+          className={({ isActive }) =>
+            `text-xl font-semibold cursor-pointer hover:scale-110 ${
+              isActive ? "text-[#1ac073]" : "hover:text-[#1ac073]"
+            }`
+          }
+        >
           Polls
-        </h1>
+        </NavLink>
       </div>
+
+      {/* Right Section */}
       <div className="right flex gap-10 items-center">
-        <img src={list} alt="" className="w-[35px] h-[35px] cursor-pointer" />
-        <img
-          src={profile}
-          alt=""
-          className="w-[35px] h-[35px] cursor-pointer"
-        />
+        <img src={list} alt="List" className="w-[35px] h-[35px] cursor-pointer" />
+        <img src={profile} alt="Profile" className="w-[35px] h-[35px] cursor-pointer" />
       </div>
     </div>
   );
