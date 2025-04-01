c1eeb3648f340bae924deff5c3e94b8ace95f981
kajalchoudhary1003 <108188712+kajalchoudhary1003@users.noreply.github.com>
2025-04-02 02:33:33 +0530

    added shadcn, made css changes in homepage, added a default google font

--- frontend/src/HomePage/page/HomePage.jsx
+++ frontend/src/HomePage/page/HomePage.jsx
@@ -1,50 +1,58 @@
-import React, { useState } from 'react'
-import HomeNavbar from '../components/HomeNavbar';
+import React, { useState } from "react";
+import HomeNavbar from "../components/HomeNavbar";
 import chef from "../../assets/chef.png";
-import FoodSelection from '../components/FoodSelection';
-import { useNavigate } from 'react-router-dom';
-import { searchDish } from '../api/zomatoApi';
+import FoodSelection from "../components/FoodSelection";
+import { useNavigate } from "react-router-dom";
+import { searchDish } from "../api/zomatoApi";
 
-const HomePage =()=> {
+const HomePage = () => {
+  const navigate = useNavigate();
 
+  const [searchInput, setSearchInput] = useState("");
 
-    const navigate = useNavigate();
+  const handleSearchClick = async (e) => {
+    e.preventDefault();
+    console.log(searchInput);
 
-    const [searchInput , setSearchInput]= useState("");
-
-    const handleSearchClick = async(e)=>{
-      e.preventDefault();
-      console.log(searchInput);
-
-      const data = await searchDish(searchInput)
-      navigate("/search" , { state: { data } })
-      
-    }
+    const data = await searchDish(searchInput);
+    navigate("/search", { state: { data } });
+  };
 
   return (
     <>
-    <HomeNavbar />
-    <section className="mid-section">
-      <div className="bg-[#1AC073] partition flex items-center justify-around">
-        <div className="paragraph">
-          <h1 className="text-4xl pb-4">Let's Order Some Food</h1>
+      <HomeNavbar />
+      <section className="mid-section">
+        <div className="bg-[#1AC073] partition py-6 rounded-bl-4xl rounded-br-4xl flex items-center justify-around">
+          <div className="paragraph">
+            <h1 className="text-4xl font-medium pb-4">Let's Order Some Food</h1>
 
-          <form onSubmit={handleSearchClick} className="search w-[620px] h-[68px]">
-            <input onChange={(e) => setSearchInput(e.target.value)}
-                value={searchInput} type="text" placeholder="Search for dish" className="bg-[#ecf0e9] outline-none text-xl pl-6  w-[440px] h-[68px] rounded-tl-xl rounded-bl-xl" />
-            <button type='submit' className="text-white bg-[#f3ba00] w-[180px] h-[68px] text-xl rounded-tr-xl rounded-br-xl cursor-pointer">Search</button>
-          </form>
-
-        </div>
-        <div className="image">
+            <form
+              onSubmit={handleSearchClick}
+              className="search w-[620px] h-[68px]"
+            >
+              <input
+                onChange={(e) => setSearchInput(e.target.value)}
+                value={searchInput}
+                type="text"
+                placeholder="Search for dish"
+                className="bg-[#ecf0e9] outline-none text-xl pl-6  w-[440px] h-[68px] rounded-tl-xl rounded-bl-xl"
+              />
+              <button
+                type="submit"
+                className="text-white bg-[#f3ba00] w-[180px] h-[68px] text-xl rounded-tr-xl rounded-br-xl cursor-pointer"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="image">
             <img src={chef} alt="" className="w-[600px] h-[550px]" />
+          </div>
         </div>
-
-      </div>
       </section>
-     <FoodSelection />
+      <FoodSelection />
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
