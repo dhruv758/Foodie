8b349a20a5e18edf4ce24aeedd6de952c7005eea
kajalchoudhary1003 <108188712+kajalchoudhary1003@users.noreply.github.com>
2025-04-24 17:18:32 +0530

    chore: Refactor DishList component for improved responsiveness and styling

--- frontend/src/Userinfo/components/DishList.jsx
+++ frontend/src/Userinfo/components/DishList.jsx
@@ -1,17 +1,18 @@
 export default function DishList({ dishes, selectedDish, setSelectedDish }) {
   return (
-    <div className="bg-white border border-gray-300 shadow-sm rounded-lg p-4 sm:p-6 ">
-      <div className="flex flex-wrap justify-start gap-4">
+    <div className="bg-white border border-gray-300 shadow-sm rounded-lg p-3 sm:p-4 lg:p-6">
+      <div className="flex flex-wrap justify-start gap-2 sm:gap-3 lg:gap-4">
         {dishes.map((dish) => (
           <button
             key={dish.id}
             onClick={() => setSelectedDish(dish.id)}
             className={`
-              h-10 sm:h-10  font-medium
+              h-8 sm:h-9 lg:h-10 font-medium
               border-2 rounded-[20px] 
-              px-4 py-1 sm:px-5 sm:py-2
+              px-2 sm:px-3 lg:px-4 py-0 sm:py-1 lg:py-1
               flex items-center justify-center cursor-pointer text-center
               transition-colors duration-200
+              text-xs sm:text-sm lg:text-base
               ${selectedDish === dish.id 
                 ? "bg-[#1ac073] border-[#1ac073] text-white hover:bg-[#18b66b]" 
                 : "border-gray-400 hover:bg-gray-100"
